411990bd03d09184bab6da3e69083c5e681a6d1c
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2025-03-06 17:13:02 +0800

    apply the config onto the Kong Gateway

--- internal/apis/service.go
+++ internal/apis/service.go
@@ -4,24 +4,26 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	kong_api "github.com/jijiechen/dami-ultra/internal/apis/kong-api"
 	"github.com/jijiechen/dami-ultra/internal/business"
 	"strings"
 )
 
 var OpenAIKey = "5VoC5nUaLiGuVpVmHuoBcoLGyQ6ezgsbyqRqXtSqw3yPDKkk7R7OJQQJ99BCACi0881XJ3w3AAAAACOGKfkY"
+var KongAdminURL = "http://ec2-54-166-250-69.compute-1.amazonaws.com:8001/routes"
 
 func WrapQuestion(question string) string {
 	return fmt.Sprintf(PromptTemplate, LuaValidator, question)
 }
 
 type DamiService struct {
-	OpenAISDK            *OpenAI
-	KongGatewayPublicUrl string
+	OpenAISDK           *OpenAI
+	KongGatewayAdminUrl string
 }
 
 func NewService() *DamiService {
 	sdk := &OpenAI{APIKey: OpenAIKey}
-	return &DamiService{OpenAISDK: sdk}
+	return &DamiService{OpenAISDK: sdk, KongGatewayAdminUrl: KongAdminURL}
 }
 
 func (s *DamiService) PostMessage(ctx context.Context, list business.MessageList) (string, error) {
@@ -42,9 +44,14 @@ func (s *DamiService) PostMessage(ctx context.Context, list business.MessageList
 	}
 
 	if respObj.Valid {
-		// (TODO) apply it
+		fmt.Println("applying configuration", respObj.RawConfiguration)
+		err = kong_api.ApplyKongConfig(s.KongGatewayAdminUrl, respObj.RawConfiguration)
+		if err != nil {
+			return "", err
+		}
+
 		return "Your configuration has been applied successfully", nil
 	} else {
-		return respObj.ErrorMessages, err
+		return "", fmt.Errorf(respObj.ErrorMessages)
 	}
 }
