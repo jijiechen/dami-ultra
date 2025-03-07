530be0e6d0a27430d3077a87867c0f562ec489cb
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2025-03-07 15:35:54 +0800

    support configuring V2_API_ENABLED and SERVER_ADDRESS

--- startup/register.go
+++ startup/register.go
@@ -4,22 +4,37 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jijiechen/dami-ultra/internal/apis"
 	"github.com/jijiechen/dami-ultra/utils/helpers"
+	"os"
 )
+
+var V2APIEnabled = os.Getenv("V2_API_ENABLED") == "true"
+var ServerListenAddr = os.Getenv("SERVER_ADDRESS")
 
 func Run(engine *gin.Engine) (err error) {
 	err = Register(engine)
 	if err != nil {
 		return
 	}
-	return engine.Run(":8080")
+
+	listenAddr := ServerListenAddr
+	if listenAddr == "" {
+		listenAddr = ":8080"
+	}
+	return engine.Run(listenAddr)
 }
 
 func Register(engine *gin.Engine) (err error) {
 	engine.RedirectTrailingSlash = true
 
-	v1 := engine.Group("/api")
+	apiEndpoints := engine.Group("/api")
 	svc := apis.NewService()
-	v1.POST("/message", helpers.BuildHandler(svc.PostMessages))
-	v1.POST("/message/v2", helpers.BuildHandler(svc.PostOperationMessage))
+	if V2APIEnabled {
+		apiEndpoints.POST("/message", helpers.BuildHandler(svc.PostOperationMessage))
+	} else {
+		apiEndpoints.POST("/message", helpers.BuildHandler(svc.PostMessages))
+	}
+
+	apiEndpoints.POST("/message/v1", helpers.BuildHandler(svc.PostMessages))
+	apiEndpoints.POST("/message/v2", helpers.BuildHandler(svc.PostOperationMessage))
 	return
 }
